2eaac87bec3b8656c1014b6898411a123fad5375
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-03-03 10:57:59 +0530

    add allowedHosts for CORS in FastAPI and Vite configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,11 +17,14 @@ app = FastAPI(
 origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "https://signature-breakdown-significantly-interesting.trycloudflare.com"
+    
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    # allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- frontend/vite.config.mts
+++ frontend/vite.config.mts
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+        allowedHosts: [
+      'signature-breakdown-significantly-interesting.trycloudflare.com'
+    ]
   }
 });
 
