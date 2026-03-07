5a52d17c44eda37c9765b490d3a3674ea3f2f2d9
Abhishek Mathur <abhishekmathur200624@gmail.com>
2026-03-07 16:18:10 +0530

    final

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://signature-breakdown-significantly-interesting.trycloudflare.com",
-    "https://skillstreet-nmtosuvo3-hackersclub24s-projects.vercel.app/",
+    "https://skillstreet-nmtosuvo3-hackersclub24s-projects.vercel.app",
     
 ]
 
